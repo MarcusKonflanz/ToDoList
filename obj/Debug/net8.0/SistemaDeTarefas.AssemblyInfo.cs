853dbf6da826827b4f4e5d682f65a3af2f04ed1e
Marcus Konflanz <vi.i.konflanz@gmail.com>
2025-12-01 20:39:25 -0300

    Add ViaCepIntegration

--- obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824fe5fd665c2fbc8e047af4a8e7ff4b8f0b5144")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
